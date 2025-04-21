b40a0669cd0678864609de09d57c9372c06aa9ea
zhoulf <807733783@qq.com>
2025-04-21 10:49:17 +0800

    fix：更换图标，调整样式

--- src/layout/components/TagsView/index.vue
+++ src/layout/components/TagsView/index.vue
@@ -280,7 +280,6 @@ function handleScroll() {
   width: 100%;
   background: var(--tags-bg, #fff);
   border-bottom: 1px solid var(--tags-item-border, #d8dce5);
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.12), 0 0 3px 0 rgba(0, 0, 0, 0.04);
 
   .tags-view-wrapper {
     .tags-view-item {
